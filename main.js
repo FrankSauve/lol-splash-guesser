792345c9d2514954f401f154281cd21645845cc0
François Crispo-Sauvé <fcrispo@stingray.com>
2025-08-20 18:29:57 -0400

    Remove form when getting it wrong

--- main.js
+++ main.js
@@ -101,12 +101,17 @@ form.addEventListener('submit', async function(event) {
         result.innerText = `❌ ${input.value} --> ${answer}`;
         resultsDiv.appendChild(result);
         input.value = "";
+        input.disabled = true;
+        input.hidden = true;
 
         const reloadBtn = document.createElement("button");
         reloadBtn.className = "m-4 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
         reloadBtn.innerText = "Retry";
         reloadBtn.onclick = () => {window.location.reload()}
         resultsDiv.appendChild(reloadBtn);
+
+        document.getElementById("submitBtn").disabled = true;
+        document.getElementById("submitBtn").hidden = true;
     }
 })
 
